7eb8d7ee8d77451b66ef44bb35cd7ac5210ccc05
Adithya Krishnan <krishsandeep@gmail.com>
2022-02-12 15:09:22 +0100

    clean repo

--- tests/scripts/repr.py
+++
@@ -1,9 +0,0 @@
-class Pizza:
-    def __init__(self, ingredients):
-        self.ingredients = ingredients
-
-    def __repr__(self):
-        return f'Pizza({self.ingredients!r})'
-
-a = Pizza(['cheese', 'tomatoes'])
-print(a)
