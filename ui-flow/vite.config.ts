29df6afd6cecbc3b09221b4e501a7f32596d2a79
Adithya Krishnan <krishsandeep@gmail.com>
2024-08-17 10:06:30 +0200

    configure and add shadcn-ui

--- ui-flow/vite.config.ts
+++ ui-flow/vite.config.ts
@@ -1,9 +1,15 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
     build: {
         outDir: "../src/barfi/static/ui-st",
     },
